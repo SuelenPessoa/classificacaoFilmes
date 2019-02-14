106510af1dab91e6b999fb36e4602ab881d0b5b0
Suelen Pessoa <suelenoliveira2111@gmail.com>
2019-02-14 20:36:42 -0200

    atualizar o código para classificacoes de filmes, percorrendo a lista com for.

--- script.js
+++ script.js
@@ -1,39 +1,15 @@
-let filmes = [{
-    nome: "capitãoAmérica", classificacao: 16
-},{nome: "barbie", classificacao: 14
-},{
-    nome:"residentEvil", classificacao: 18
-},{
-    nome: "mulherMaravilha", classificacao: 18
-},{
-    nome: "minions", classificacao: 14
-}];
+let filmes = [
+    {titulo: 'como treinar seu dragão', classificacao: '0'},
+    {titulo: 'Vidro', classificacao: '12'},
+    {titulo: 'Batman O Cavaleiro das trevas', classificacao: '14'},
+    {titulo: 'Resident Evil', classificacao: '16'},
+    {titulo: 'Anjos da Noite', classificacao: '18'},
+];
 
-let acc = "";
-let idade =Number(prompt("Voce pode assistir"));
+let idade = Number(prompt('Por favor digite sua idade : '));
 
-if (idade <=filmes[0].classificacao){
-    acc += `você pode assistir ${filmes[0].nome} 
-     `;
-};
-
-if (idade >=filmes[1].classificacao){
-    acc +=  `você pode assistir ${filmes[1].nome}
-    `;
-};
-
-if (idade <=filmes[2].classificacao){   
-    acc += `você pode assistir ${filmes[2].nome}
-    ` ;
-};
-
-if (idade <=filmes[3].classificacao){
-     acc +=  `você pode assistir ${filmes[3].nome}
-     `;
-};
-  
-if (idade <=filmes[4].classificacao){   
-    acc +=  `você pode assistir ${filmes[4].nome}
-    `;
+for (let item in filmes){
+    if (idade >= filmes[item].classificacao){
+        console.log(filmes[item].titulo);
+    }
 }
-    alert(acc);
